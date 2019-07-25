086398feae999e06539a285bdf236df15676ee89
Mateusz Bugowski <mateusz.bugowski@gmail.com>
2019-07-25 18:59:08 +0200

    update routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  root 'orders#index'
-  resources :orders, only: [:index, :show, :new, :create] do
-    member do
-      post :pay
+  root 'react_app#index'
+  namespace :api, defaults: { format: 'json' } do
+    resources :orders, only: [:index, :show, :new, :create] do
+      member do
+        post :pay
+      end
     end
+    get :product_customer_options, to: 'utils#product_customer_options'
+    resources :deliveries, only: [:update]
+    mount RailsEventStore::Browser => '/res'
   end
-  resources :deliveries, only: [:update]
-  mount RailsEventStore::Browser => '/res'
+
+  get "*path", to: "react_app#index", via: :all, constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
