c347df8061124a6b430af2009e2148e2c22eac43
Mateusz Bugowski <mateusz.bugowski@gmail.com>
2019-07-25 21:35:27 +0200

    unify names

--- app/javascript/packs/application.jsx
+++ app/javascript/packs/application.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import Root from 'src/components/Root';
 import 'normalize.css/normalize.css';
-import 'styles/app.scss';
+import 'styles/application.scss';
 
 ReactDOM.render(<Root />, document.getElementById('app'));
